c3587a4dce8ad6a8cf627429995bbedc5d314de0
Andrea Bonetti <andreabonetti@gmail.com>
2025-07-04 17:54:00 +0200

    list scholar as markdown list

--- examples/example_scholar.py
+++ examples/example_scholar.py
@@ -41,18 +41,20 @@ if __name__ == "__main__":
         scholar.append([subject_improved, snippet_improved])
 
     # get length of longest subject string
-    max_len = 0
-    for s in scholar:
-        max_len = max(max_len, len(s[0]))
-    max_len += 2 # some margin for better printing
+    # max_len = 0
+    # for s in scholar:
+    #     max_len = max(max_len, len(s[0]))
+    # max_len += 2 # some margin for better printing
 
     # scholar text
     scholar_text=''
     for s in scholar:
-        scholar_text += f"- {s[0]:<{max_len}}{s[1]}\n"
+        # scholar_text += f"- {s[0]:<{max_len}}{s[1]}\n"
+        scholar_text += f"- **{s[0]}**: {s[1]}\n"
 
     scholar_text_before_llm = copy.deepcopy(scholar_text)
 
+    
     # model = 'gemma3:4b'
     model = 'gemma3:12b'
 
@@ -60,34 +62,34 @@ if __name__ == "__main__":
     print('Running: LLM parsing...')
     prompt = f"""
     The text below is a bullet point list.
-    Each bullet point reports the reference author, padding space, the title, the complete list of authors, and additional information.
+    Each bullet point reports the reference author in bold, the title, the complete list of authors, and additional information.
     Remove the complete list of authors and the additional information **after** the title of each bullet point.
-    Keep reference author, padding space, and the title, as they are now.
+    Keep the reference author and the title, as they are now.
 
     Example of input bullet point:
-    - Subhasish Mitra      Generalized qed pre-silicon verification framework S Mitra, C BARRETT, CJ Trippel, S Chattopadhyay - US Patent App. 18/541722, 2025 Abstract Systems and methods of verifying a hardware processing
+    - **Subhasish Mitra**: Generalized qed pre-silicon verification framework S Mitra, C BARRETT, CJ Trippel, S Chattopadhyay - US Patent App. 18/541722, 2025 Abstract Systems and methods of verifying a hardware processing
 
     Output should be:
-    - Subhasish Mitra      Generalized qed pre-silicon verification framework
+    - **Subhasish Mitra**: Generalized qed pre-silicon verification framework
 
     Return the modified text without any comment or request from you.
     {scholar_text_before_llm}
     """
     scholar_text_after_llm = ollama(prompt=prompt, model=model)
 
-    # llm summary
-    print('Running: LLM summary...')
-    prompt = f"""
-    Summarize the content of these papers.
-    Do not ask questions or add comments.
-    {scholar_text_before_llm}
-    """
-    scholar_summary = ollama(prompt=prompt, model=model)
+    # # llm summary
+    # print('Running: LLM summary...')
+    # prompt = f"""
+    # Summarize the content of these papers.
+    # Do not ask questions or add comments.
+    # {scholar_text_before_llm}
+    # """
+    # scholar_summary = ollama(prompt=prompt, model=model)
 
     # add headers
     scholar_text_before_llm = 'Google Scholar - New Articles\n' + scholar_text_before_llm
     scholar_text_after_llm = 'Google Scholar - New Articles\n' + scholar_text_after_llm
-    scholar_summary = 'Google Scholar - Summary\n' + scholar_summary
+    # scholar_summary = 'Google Scholar - Summary\n' + scholar_summary
 
     # print
     print('')
@@ -95,5 +97,5 @@ if __name__ == "__main__":
     print('')
     print(scholar_text_after_llm)
     print('')
-    print(scholar_summary)
-    print('')
+    # print(scholar_summary)
+    # print('')
