fc8a24165a4a379c00e0986d985e7c9792e024fd
Andrea Bonetti <andreabonetti@gmail.com>
2025-07-16 23:57:09 +0200

    removed obsolete example

--- examples/example_print_summary.py
+++
@@ -1,7 +0,0 @@
-"""Get all labels from your Gmail"""
-
-from parsemind import get_summary
-
-if __name__ == '__main__':
-    # using debug mode for faster operation
-    get_summary(scholar=True, do_print=True, verbose=True, debug=True)
